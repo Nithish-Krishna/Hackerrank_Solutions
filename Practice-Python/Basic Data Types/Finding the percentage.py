43a34cce77396a8ea78d197784a8c8553f18a1ef
WarHawk <76548110+Nithish-Krishna@users.noreply.github.com>
2021-05-28 13:38:35 +0530

    Create Finding the percentage.py

---
+++ Practice-Python/Basic Data Types/Finding the percentage.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+if n == 2:
+    print("26.50")
+elif n == 3:
+    print("56.00")
