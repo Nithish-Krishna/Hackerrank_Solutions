079d0f5a9ea2b15331e33777ff1d1bb1cb1caaa0
WarHawk <76548110+Nithish-Krishna@users.noreply.github.com>
2021-05-29 15:19:12 +0530

    Update String Split and Join.py

--- Practice-Python/Strings/String Split and Join.py
+++ Practice-Python/Strings/String Split and Join.py
@@ -1,1 +1,10 @@
+def split_and_join(line):
+    x=line.split(" ") 
+    x='-'.join(x)    
+    return x
 
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
